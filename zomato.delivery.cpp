9ebd04cd5b142d5d6be5d84f9f0ac154253822a8
aashi1408 <aashiv2525@gmail.com>
2025-11-25 20:43:20 +0530

    Update zomato.delivery.cpp

--- zomato.delivery.cpp
+++ zomato.delivery.cpp
@@ -4,56 +4,36 @@ the charges taken are as follows:
 2.if the destination is beyond every km over 5km ; the company charges 2 rupees per kilometer.
 3.18% taxes are also added over the cart amount(product)
 4.if it is raining 20 rupees extra is charged*/
-//#include<stdio.h>
-//int main(){
-//	//inputs
-//	//; is necessary if their are different data types
-//	float cart_cost;float distance;char rain;
-//	float payable;
-//	printf("enter cart cost: ");
-//	scanf("%f",&cart_cost);
-//	//after adding 18% GST
-//	cart_cost = cart_cost + cart_cost*0.18 ;//1*18% i.e.->1.18
-//	printf("enter the distance : ");
-//	scanf("%f",&distance);
-//	//if distance is less or equal to 5 then we will consider that distance as 5
-//	if(distance<=5){
-//		cart_cost = cart_cost + 20;
-//	}
-//	else{
-//		cart_cost = cart_cost + 20 + (distance-5)*2;
-//	}
-//	printf("is it raining? (y/n) : ");
-//	fflush(stdin);//imp if we input char after int
-//	scanf(" %c",&rain);
-//	if(rain=='y'){
-//		cart_cost += 20;
-//	}
-//	printf("Please pay : Rs. %.2f/-",cart_cost);
-//}
-
-#include<iostream>
-using namespace std;
+#include<stdio.h>
 int main(){
-	int num_of_elements;
-	cout<<"enter number of elements in array : ";
-	cin>>num_of_elements;
-	int arr[num_of_elements];
-	cout<<"enter elements : ";
-	for(int i=0;i<num_of_elements;i++){
-		cin>>arr[i];
+	//inputs
+	//; is necessary if their are different data types
+	float cart_cost;float distance;char rain;
+	float payable;
+	printf("enter cart cost: ");
+	scanf("%f",&cart_cost);
+	//after adding 18% GST
+	cart_cost = cart_cost + cart_cost*0.18 ;//1*18% i.e.->1.18
+	printf("enter the distance : ");
+	scanf("%f",&distance);
+	//if distance is less or equal to 5 then we will consider that distance as 5
+	if(distance<=5){
+		cart_cost = cart_cost + 20;
 	}
-	for(int i=0;i<num_of_elements;i++){
-		cout<<arr[i]<<" ";
+	else{
+		cart_cost = cart_cost + 20 + (distance-5)*2;
 	}
-	cout<<endl;
-	 
-	int mx = arr[0];
-	for(int i=1;i<num_of_elements;i++){
-		mx = max(mx,arr[i]);
+	printf("is it raining? (y/n) : ");
+	fflush(stdin);//imp if we input char after int
+	scanf(" %c",&rain);
+	if(rain=='y'){
+		cart_cost += 20;
 	}
-	cout<<"maximum of all elements : "<<mx;
+	printf("Please pay : Rs. %.2f/-",cart_cost);
 }
+
+
+
 
 
 
