42bea96919539adff52eecc7ea9ea56c2c52a164
aashi1408 <aashiv2525@gmail.com>
2025-11-28 23:59:45 +0530

    Add files via upload

---
+++ AREA_OF_CIRCLE.cpp
@@ -0,0 +1,14 @@
+//Area_of_circle
+
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+#define PI 3.141592653
+
+int main()
+{
+	double r;
+	scanf("%lf",&r);
+	printf("%.9lf",PI*r*r);
+	return 0;
+}
