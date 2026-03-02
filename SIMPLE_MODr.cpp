3ad1349fe0f28e952b66e92a0c7860da48615ad5
aashi1408 <aashiv2525@gmail.com>
2026-03-02 17:28:18 +0530

    Add files via upload

---
+++ SIMPLE_MODr.cpp
@@ -0,0 +1,14 @@
+//SIMPLE MOD
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+//	input size of N : 10 ^ 100
+	string N;
+	cin>>N;
+	cout<<0<<" "<<0;
+	//they are asking non negative X,Y mod N == 0 so use 0 as both!
+	return 0;
+}
